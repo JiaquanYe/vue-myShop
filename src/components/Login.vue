e510f81057b1a7dcd1a693d3187d69ac4b570fff
YeJQ <8034753+jiaquanye@user.noreply.gitee.com>
2020-09-26 16:08:14 +0800

    modify eslintrc rules.

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,19 +6,32 @@
         <img src="../assets/logo.png" alt="" />
       </div>
       <!--表单区域-->
-      <el-form ref="loginFormRef" :rules="loginFormRules" :model="loginForm" label-width="0px" class="login_form">
+      <el-form
+        ref="loginFormRef"
+        :rules="loginFormRules"
+        :model="loginForm"
+        label-width="0px"
+        class="login_form"
+      >
         <!--用户名-->
         <el-form-item prop="username">
-            <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
+          <el-input
+            v-model="loginForm.username"
+            prefix-icon="iconfont icon-user"
+          ></el-input>
         </el-form-item>
         <!--用户名-->
         <el-form-item prop="password">
-            <el-input type="password" v-model="loginForm.password" prefix-icon="iconfont icon-3702mima"></el-input>
+          <el-input
+            type="password"
+            v-model="loginForm.password"
+            prefix-icon="iconfont icon-3702mima"
+          ></el-input>
         </el-form-item>
         <!--登录、重置按钮区域-->
         <el-form-item class="btns">
-            <el-button type="primary" @click="login">登录</el-button>
-            <el-button type="info" @click="resetLoginForm">重置</el-button>
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -27,56 +40,56 @@
 
 <script>
 export default {
-    data() {
-        return {
-            // 登录表单数据绑定
-            loginForm: {
-                username: 'admin',
-                password: '123456'
-            },
-            // 表单验证规则
-            loginFormRules: {
-                // 验证用户名合法性
-                username: [
-                    { required: true, message: '请输入登录名称', trigger: 'blur' },
-                    { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
-                ],
-                // 验证密码合法性
-                password: [
-                    { required: true, message: '请输入登录密码', trigger: 'blur' },
-                    { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
-                ]
-            }
-        }
-    },
-    methods: {
-        // 点击重置登录表单
-        resetLoginForm() {
-            // console.log(this)
-            this.$refs.loginFormRef.resetFields()
-        },
-        login() {
-            // validate函数接收一个回调函数作为参数
-            this.$refs.loginFormRef.validate(async (valid) => {
-                // 逻辑判断
-                if (!valid) return
-                // login是请求地址：根路径+/login
-                // this.loginForm是包含请求参数username, password的对象
-                // await与async配合使用，简化axios返回数据输出
-                // { data: res } 仅去返回数据中data选项
-                const { data: res } = await this.$http.post('login', this.loginForm)
-                if (res.meta.status !== 200) return this.$message.error('登录失败!')
-                this.$message.success('登录成功')
-                // 登录成功后， 将token保存到sessionStorage中
-                // token的作用是，用它来访问其他网页API接口
-                // token应仅在当前网站打开时生效，所以将token保存在sessionStorage中
-                console.log(res)
-                window.sessionStorage.setItem('token', res.data.token)
-                // 登录成功后，通过编程式导航跳转到后台主页，路由地址式/home
-                this.$router.push('/home')
-            })
-        }
+  data() {
+    return {
+      // 登录表单数据绑定
+      loginForm: {
+        username: 'admin',
+        password: '123456'
+      },
+      // 表单验证规则
+      loginFormRules: {
+        // 验证用户名合法性
+        username: [
+          { required: true, message: '请输入登录名称', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ],
+        // 验证密码合法性
+        password: [
+          { required: true, message: '请输入登录密码', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+        ]
+      }
     }
+  },
+  methods: {
+    // 点击重置登录表单
+    resetLoginForm() {
+      // console.log(this)
+      this.$refs.loginFormRef.resetFields()
+    },
+    login() {
+      // validate函数接收一个回调函数作为参数
+      this.$refs.loginFormRef.validate(async valid => {
+        // 逻辑判断
+        if (!valid) return
+        // login是请求地址：根路径+/login
+        // this.loginForm是包含请求参数username, password的对象
+        // await与async配合使用，简化axios返回数据输出
+        // { data: res } 仅去返回数据中data选项
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        if (res.meta.status !== 200) return this.$message.error('登录失败!')
+        this.$message.success('登录成功')
+        // 登录成功后， 将token保存到sessionStorage中
+        // token的作用是，用它来访问其他网页API接口
+        // token应仅在当前网站打开时生效，所以将token保存在sessionStorage中
+        console.log(res)
+        window.sessionStorage.setItem('token', res.data.token)
+        // 登录成功后，通过编程式导航跳转到后台主页，路由地址式/home
+        this.$router.push('/home')
+      })
+    }
+  }
 }
 </script>
 
@@ -114,14 +127,14 @@ export default {
   }
 }
 .btns {
-    display: flex;
-    justify-content: flex-end;
+  display: flex;
+  justify-content: flex-end;
 }
 .login_form {
-    position: absolute;
-    bottom: 0;
-    width: 100%;
-    padding: 0 20px;
-    box-sizing: border-box;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,21 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import './plugins/element.js'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./plugins/element.js";
 // 导入字体图标
-import './assets/fonts/iconfont.css'
+import "./assets/fonts/iconfont.css";
 // 导入全局样式
-import './assets/css/global.css'
+import "./assets/css/global.css";
 
-import axios from 'axios'
+import axios from "axios";
 // 配置请求的根路径
-axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.defaults.baseURL = "http://127.0.0.1:8888/api/private/v1/";
 // 把axios挂载在Vue的原型,使其可通过Vue.$http访问到axios
-Vue.prototype.$http = axios
+Vue.prototype.$http = axios;
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
