a6f1755752a50dd4be11fd5cc222a3ee2d9af4c0
YeJQ <8034753+jiaquanye@user.noreply.gitee.com>
2020-10-08 22:20:47 +0800

    bulid home.vue architecture.

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,11 +1,32 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import {
+  Button,
+  Form,
+  FormItem,
+  Input,
+  Message,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Menu,
+  Submenu,
+  MenuItemGroup,
+  MenuItem
+} from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
-
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItemGroup)
+Vue.use(MenuItem)
 // 把Message挂载到Vue的原型对象上,后面可通过this，访问$message,从而访问Message
 // $message中的message是自定义的，合法即可
 Vue.prototype.$message = Message
